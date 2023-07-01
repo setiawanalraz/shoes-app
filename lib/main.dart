ed350a95eda364bba04fa91457f496281aee856d
Alraz Setiawan <alrazsetiawan79@gmail.com>
2023-07-02 00:49:02 +0700

    Creating detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoes_app/pages/detail_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DetailPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
